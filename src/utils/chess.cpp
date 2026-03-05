b8bcebefbbef59cb01007862c3e655d6042de68b
Parker-Isaacson <parker.isaacson@gmail.com>
2026-03-05 18:11:50 -0500

    Updated design of utility functions

--- src/utils/chess.cpp
+++ src/utils/chess.cpp
@@ -6,142 +6,57 @@
 
 std::vector<std::pair<int, int>> get_piece_moves(Piece piece) {
     switch (piece) {
-        case Piece::White_King:
-        case Piece::Black_King:
-            return {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}; // Does not include castling
-
-        case Piece::White_Queen:
-        case Piece::Black_Queen:
-            return {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}, {2, 0}, {-2, 0}, {0, 2}, {0, -2}, {2, 2}, {-2, -2}, {2, -2}, {-2, 2}, {3, 0}, {-3, 0}, {0, 3}, {0, -3}, {3, 3}, {-3, -3}, {3, -3}, {-3, 3}, {4, 0}, {-4, 0}, {0, 4}, {0, -4}, {4, 4}, {-4, -4}, {4, -4}, {-4, 4}, {5, 0}, {-5, 0}, {0, 5}, {0, -5}, {5, 5}, {-5, -5}, {5, -5}, {-5, 5}, {6, 0}, {-6, 0}, {0, 6}, {0, -6}, {6, 6}, {-6, -6}, {6, -6}, {-6, 6}, {7, 0}, {-7, 0}, {0, 7}, {0, -7}, {7, 7}, {-7, -7}, {7, -7}, {-7, 7}};
-
-        case Piece::White_Rook:
-        case Piece::Black_Rook:
-            return {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {2, 0}, {-2, 0}, {0, 2}, {0, -2}, {3, 0}, {-3, 0}, {0, 3}, {0, -3}, {4, 0}, {-4, 0}, {0, 4}, {0, -4}, {5, 0}, {-5, 0}, {0, 5}, {0, -5}, {6, 0}, {-6, 0}, {0, 6}, {0, -6}, {7, 0}, {-7, 0}, {0, 7}, {0, -7}};
-
-        case Piece::White_Bishop:
-        case Piece::Black_Bishop:
-            return {{1, 1}, {-1, -1}, {1, -1}, {-1, 1}, {2, 2}, {-2, -2}, {2, -2}, {-2, 2}, {3, 3}, {-3, -3}, {3, -3}, {-3, 3}, {4, 4}, {-4, -4}, {4, -4}, {-4, 4}, {5, 5}, {-5, -5}, {5, -5}, {-5, 5}, {6, 6}, {-6, -6}, {6, -6}, {-6, 6}, {7, 7}, {-7, -7}, {7, -7}, {-7, 7}};
-
-        case Piece::White_Knight:
-        case Piece::Black_Knight:
-            return {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
-
-        case Piece::White_Pawn:
-        case Piece::Black_Pawn:
-            return {{1, 0}}; // Does not include two moves forward, will be include if white is on the 2nd rank, or if black is on the 7th.
-
-        case Piece::Empty:
-        default:
-            return {};
+        case Piece::White_King: case Piece::Black_King: return {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}; // Does not include castling
+        case Piece::White_Queen: case Piece::Black_Queen: return {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}, {2, 0}, {-2, 0}, {0, 2}, {0, -2}, {2, 2}, {-2, -2}, {2, -2}, {-2, 2}, {3, 0}, {-3, 0}, {0, 3}, {0, -3}, {3, 3}, {-3, -3}, {3, -3}, {-3, 3}, {4, 0}, {-4, 0}, {0, 4}, {0, -4}, {4, 4}, {-4, -4}, {4, -4}, {-4, 4}, {5, 0}, {-5, 0}, {0, 5}, {0, -5}, {5, 5}, {-5, -5}, {5, -5}, {-5, 5}, {6, 0}, {-6, 0}, {0, 6}, {0, -6}, {6, 6}, {-6, -6}, {6, -6}, {-6, 6}, {7, 0}, {-7, 0}, {0, 7}, {0, -7}, {7, 7}, {-7, -7}, {7, -7}, {-7, 7}};
+        case Piece::White_Rook: case Piece::Black_Rook: return {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {2, 0}, {-2, 0}, {0, 2}, {0, -2}, {3, 0}, {-3, 0}, {0, 3}, {0, -3}, {4, 0}, {-4, 0}, {0, 4}, {0, -4}, {5, 0}, {-5, 0}, {0, 5}, {0, -5}, {6, 0}, {-6, 0}, {0, 6}, {0, -6}, {7, 0}, {-7, 0}, {0, 7}, {0, -7}};
+        case Piece::White_Bishop: case Piece::Black_Bishop: return {{1, 1}, {-1, -1}, {1, -1}, {-1, 1}, {2, 2}, {-2, -2}, {2, -2}, {-2, 2}, {3, 3}, {-3, -3}, {3, -3}, {-3, 3}, {4, 4}, {-4, -4}, {4, -4}, {-4, 4}, {5, 5}, {-5, -5}, {5, -5}, {-5, 5}, {6, 6}, {-6, -6}, {6, -6}, {-6, 6}, {7, 7}, {-7, -7}, {7, -7}, {-7, 7}};
+        case Piece::White_Knight: case Piece::Black_Knight: return {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
+        case Piece::White_Pawn: case Piece::Black_Pawn: return {{1, 0}}; // Does not include two moves forward, will be include if white is on the 2nd rank, or if black is on the 7th.
+        case Piece::Empty: default: return {};
     }
 }
 
 char piece_to_string(Piece p) {
     switch (p) {
-        case Piece::White_King:
-            return 'K';
-
-        case Piece::White_Queen:
-            return 'Q';
-
-        case Piece::White_Bishop:
-            return 'B';
-
-        case Piece::White_Knight:
-            return 'N';
-
-        case Piece::White_Rook:
-            return 'R';
-
-        case Piece::White_Pawn:
-            return 'P';
-
-        case Piece::Black_King:
-            return 'k';
-
-        case Piece::Black_Queen:
-            return 'q';
-
-        case Piece::Black_Bishop:
-            return 'b';
-
-        case Piece::Black_Knight:
-            return 'n';
-
-        case Piece::Black_Rook:
-            return 'r';
-
-        case Piece::Black_Pawn:
-            return 'p';
-
-        default:
-            return 'z';
+        case Piece::White_King: return 'K';
+        case Piece::White_Queen: return 'Q';
+        case Piece::White_Bishop: return 'B';
+        case Piece::White_Knight: return 'N';
+        case Piece::White_Rook: return 'R';
+        case Piece::White_Pawn: return 'P';
+        case Piece::Black_King: return 'k';
+        case Piece::Black_Queen: return 'q';
+        case Piece::Black_Bishop: return 'b';
+        case Piece::Black_Knight: return 'n';
+        case Piece::Black_Rook: return 'r';
+        case Piece::Black_Pawn: return 'p';
+        default: return 'z';
     }
 }
 
 Piece string_to_piece(char c) {
     switch (c) {
-        case 'K':
-            return Piece::White_King;
-
-        case 'Q':
-            return Piece::White_Queen;
-
-        case 'B':
-            return Piece::White_Bishop;
-
-        case 'N':
-            return Piece::White_Knight;
-
-        case 'R':
-            return Piece::White_Rook;
-
-        case 'P':
-            return Piece::White_Pawn;
-
-        case 'k':
-            return Piece::Black_King;
-
-        case 'q':
-            return Piece::Black_Queen;
-
-        case 'b':
-            return Piece::Black_Bishop;
-
-        case 'n':
-            return Piece::Black_Knight;
-
-        case 'r':
-            return Piece::Black_Rook;
-
-        case 'p':
-            return Piece::Black_Pawn;
-
-        default:
-            return Piece::Empty;
+        case 'K': return Piece::White_King;
+        case 'Q': return Piece::White_Queen;
+        case 'B': return Piece::White_Bishop;
+        case 'N': return Piece::White_Knight;
+        case 'R': return Piece::White_Rook;
+        case 'P': return Piece::White_Pawn;
+        case 'k': return Piece::Black_King;
+        case 'q': return Piece::Black_Queen;
+        case 'b': return Piece::Black_Bishop;
+        case 'n': return Piece::Black_Knight;
+        case 'r': return Piece::Black_Rook;
+        case 'p': return Piece::Black_Pawn;
+        default: return Piece::Empty;
     }
 }
 
 Side Game::side_of_piece(Piece p) {
     switch (p) {
-        case Piece::White_King:
-        case Piece::White_Queen:
-        case Piece::White_Bishop:
-        case Piece::White_Knight:
-        case Piece::White_Rook:
-        case Piece::White_Pawn:
-            return Side::White;
-
-        case Piece::Black_King:
-        case Piece::Black_Queen:
-        case Piece::Black_Bishop:
-        case Piece::Black_Knight:
-        case Piece::Black_Rook:
-        case Piece::Black_Pawn:
-            return Side::Black;
-
-  default:
-            return Side::Empty;
+        case Piece::White_King: case Piece::White_Queen: case Piece::White_Bishop: case Piece::White_Knight: case Piece::White_Rook: case Piece::White_Pawn: return Side::White;
+        case Piece::Black_King: case Piece::Black_Queen: case Piece::Black_Bishop: case Piece::Black_Knight: case Piece::Black_Rook: case Piece::Black_Pawn: return Side::Black;
+        default: return Side::Empty;
     }
 }
 
